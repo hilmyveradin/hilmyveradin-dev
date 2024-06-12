7431c6a1f96edbc754f58a264ff1b3e1181cfcc0
hilmyveradin <hilmyveradin@gmail.com>
2024-06-12 11:21:55 +0700

    fix linting issues

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,7 +31,7 @@ export default function Projects() {
           ))}
         </div>
       </div>
-      <div className="space-y-2 pt-6 md:space-y-5 mt-4">
+      <div className="mt-4 space-y-2 pt-6 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Projects
         </h1>
